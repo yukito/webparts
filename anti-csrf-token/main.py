d2ff8bfe8a94e95c2ca75dee552ed376f9292adb
yukito <yuki.ito.206@gmail.com>
2016-11-19 11:40:37 +0900

    implemented anti-csrf-token

--- anti-csrf-token/main.py
+++ anti-csrf-token/main.py
@@ -2,12 +2,22 @@
 # coding:utf-8
 
 from flask import Flask, render_template, request, redirect, url_for
+import uuid
 
 app = Flask(__name__)
 
 @app.before_request
-def set_csrf_protection():
+def csrf_protection():
    if request.method == "POST":
+      token = session.pop('_csrf_token', None)
+         if not token or token != request.form.get('_csrf_token'):
+            abort(403)
+
+def generate_csrf_token():
+   session['_csrf_token'] = uuid.uuid4()
+   return session['_csrf_token']
+
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
 if __name__ == '__main__':
    app.debug = True
